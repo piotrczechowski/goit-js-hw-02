98d607c66c8e2899e9255c2a844c718f6c78b661
Piotr Czechowski <czechowp@gmail.com>
2024-10-03 20:04:52 +0200

    tas 2

--- js/task-2.js
+++ js/task-2.js
@@ -15,4 +15,3 @@ console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula 
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
